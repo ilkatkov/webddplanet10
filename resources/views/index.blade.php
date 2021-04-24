e390652e8787938d99e0ee9385005605e8b662b4
ilkatkov <ilkatkov@yandex.ru>
2021-04-25 01:42:30 +0300

    Added favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Служба поддержки</title>
+    <link rel="shortcut icon" href="icon.ico" type="image/x-icon">
     <script
         src="scripts/jquery-3.5.1.min.js">
     </script>
